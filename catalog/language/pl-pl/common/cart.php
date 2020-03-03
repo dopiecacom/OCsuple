22fb679d410b9d6caf4b33a87ae568608f9cf790
yszty <ysztykox@gmail.com>
2020-03-03 12:45:26 +0100

    cart red circle

--- catalog/language/pl-pl/common/cart.php
+++ catalog/language/pl-pl/common/cart.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_items']     = 'Twój koszyk';
+$_['text_items']     = '%s';
 $_['text_empty']     = 'Koszyk jest pusty!';
 $_['text_cart']      = 'Zobacz koszyk';
 $_['text_checkout']  = 'Przejdź do kasy';
